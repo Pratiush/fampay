e064e677f79a304b43d6b159d95ab7a6da0b16c4
pratiush <pratiush309@gmail.com>
2020-04-27 00:28:58 +0530

    paginated response

--- fampay/famapi/urls.py
+++ fampay/famapi/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 
+router = DefaultRouter()
+router.register(r'videos', views.VideoViewSet)
 
 urlpatterns = [
     path('ping/', views.healthCheck, name='ping'),
+    path('',include(router.urls)),
 ]
